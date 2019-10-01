e12490322d701f96b7bb4e3605400c2c650c18ee
Hongliang Dai <hldai@outlook.com>
2019-10-01 21:02:15 +0800

    update

--- misc.py
+++ misc.py
@@ -13,6 +13,21 @@ def __text_from_anchor_sents_file(anchor_sents_file, output_file):
     fout.close()
 
 
+def merge_files(filenames, output_file):
+    fout = open(output_file, 'w', encoding='utf-8', newline='\n')
+    for filename in filenames:
+        print(filename)
+        f = open(filename, encoding='utf-8')
+        for line in f:
+            fout.write(line)
+        f.close()
+    fout.close()
+
+
 wiki19_anchor_sents_file = 'd:/data/res/wiki/anchor/enwiki-20190101-anchor-sents.txt'
 anchor_sent_texts_file = 'd:/data/res/wiki/anchor/enwiki-20190101-anchor-sents-tok-texts.txt'
-__text_from_anchor_sents_file(wiki19_anchor_sents_file, anchor_sent_texts_file)
+# __text_from_anchor_sents_file(wiki19_anchor_sents_file, anchor_sent_texts_file)
+
+part_pos_tag_files = [f'd:/data/res/wiki/anchor/enwiki-20190101-anchor-sents-tok-texts-pos-{i}.txt' for i in range(4)]
+pos_tag_file = 'd:/data/res/wiki/anchor/enwiki-20190101-anchor-sents-tok-texts-pos.txt'
+# merge_files(part_pos_tag_files, pos_tag_file)
